160484073fbc282d04e18baef1226ef1b07c43be
Almerindo Rehem <almerindo@almerindo-tux>
2020-01-21 03:13:12 -0300

    chore: Adicionados testes para assinatura de plano

--- sanarPay/server/__tests__/integration/Customers.test.js
+++ sanarPay/server/__tests__/integration/Customers.test.js
@@ -134,41 +134,13 @@ describe('CUSTOMERS', () => {
       });
   });
 
-  it('Deve se logar na API REST e LISTAR os cartoes em sua WALLET', async () => {
-    data = {};
-    data.name = 'Juliana';
-    data.email = 'juliana@sanarflix.com.br';
-    data.password = '1234567890';
-
-    // Se loga como o usuário e captura o token
-    await request(app)
-      .post('/sessions')
-      .send(data)
-      .then(response => {
-        data.token = response.body.token;
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('token');
-        const { customer } = response.body;
-        data.id = customer.id;
-      });
-
-    await request(app)
-      .get(`${url}/wallet`)
-      .set('Authorization', `Bearer ${data.token}`)
-      .send(data)
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toHaveProperty('cards');
-      });
-  });
-
   it('Deve Criar um Customer local e remoto (MundiPagG) e se logar na API REST e ADICIONAR um cartão em sua WALLET e APAGAR O CARTAO', async () => {
     data = {};
-    data.name = 'Andre';
-    data.email = 'andre@sanarflix.com.br';
+    data.name = 'Carolina Carla';
+    data.email = 'carolinacarla@sanarflix.com.br';
     data.password = '1234567890';
 
-    // Cria o usuário Juliana
+    // Cria o usuário Carolina
     await request(app)
       .post(url)
       .send(data)
@@ -222,6 +194,171 @@ describe('CUSTOMERS', () => {
       .then(response => {
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty('remoteCard');
+      });
+  });
+
+  it('Deve se logar na API REST e LISTAR os cartoes em sua WALLET', async () => {
+    data = {};
+    data.name = 'Juliana';
+    data.email = 'juliana@sanarflix.com.br';
+    data.password = '1234567890';
+
+    // Se loga como o usuário e captura o token
+    await request(app)
+      .post('/sessions')
+      .send(data)
+      .then(response => {
+        data.token = response.body.token;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+        const { customer } = response.body;
+        data.id = customer.id;
+      });
+
+    await request(app)
+      .get(`${url}/wallet`)
+      .set('Authorization', `Bearer ${data.token}`)
+      .send(data)
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('cards');
+      });
+  });
+
+  let plan_remote_id;
+  it('Deve se locar como usuário Admin e criar o plano Standard por R$ 24,50', async () => {
+    data = {};
+    data.email = 'admin@sanarflix.com.br';
+    data.password = '1234567890';
+    await request(app)
+      .post('/sessions')
+      .send(data)
+      .then(response => {
+        data.token = response.body.token;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+      });
+    // enviando os dados do plano e o token
+    data = {
+      token: data.token,
+      name: 'Plano Standard',
+      currency: 'BRL',
+      interval: 'month',
+      interval_count: 1,
+      billing_type: 'prepaid',
+      minimum_price: 2450,
+      installments: [1],
+      payment_methods: ['credit_card', 'boleto'],
+      items: [
+        {
+          name: 'Sanar Flix',
+          quantity: 1,
+          pricing_scheme: {
+            price: 2450,
+          },
+        },
+      ],
+    };
+    await request(app)
+      .post('/plans')
+      .set('Authorization', `Bearer ${data.token}`)
+      .send(data)
+      .then(response => {
+        plan_remote_id = response.body.remote_id;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('remote_id');
+      });
+  });
+
+  it('Deve Criar um Customer Mario ', async () => {
+    data = {};
+    data.name = 'Mario Santos';
+    data.email = 'mariosantos@gmail.com';
+    data.password = '1234567890';
+
+    await request(app)
+      .post('/customers')
+      .send(data)
+      .then(response => {
+        data.id = response.body.id;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('remote_id');
+      });
+
+    // return request(app)
+    //   .post('/sessions')
+    //   .send(data)
+    //   .then(response => {
+    //     expect(response.statusCode).toBe(200);
+    //     expect(response.body).toHaveProperty('token');
+    //   });
+  });
+
+  it('Mario deve fazer o login', async () => {
+    data = {};
+    data.name = 'Mario Santos';
+    data.email = 'mariosantos@gmail.com';
+    data.password = '1234567890';
+
+    return request(app)
+      .post('/sessions')
+      .send(data)
+      .then(response => {
+        data.token = response.body.token;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+      });
+  });
+
+  it('Mario deve cadastrar seu cartao na wallet', async () => {
+    // Utiliza esse mesmo token para criar um cartao em sua wallet
+    data = {
+      token: data.token,
+    };
+
+    data.card = {
+      number: '4011185771285580',
+      holder_name: 'Tony Stark',
+      holder_document: '93095135270',
+      exp_month: 1,
+      exp_year: 20,
+      cvv: '651',
+      brand: 'Mastercard',
+      private_label: false,
+      options: {
+        verify_card: true,
+      },
+    };
+
+    await request(app)
+      .post(`/customers/wallet`)
+      .set('Authorization', `Bearer ${data.token}`)
+      .send(data)
+      .then(response => {
+        data.card.remote_id = response.body.remote_id;
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('remote_id');
+      });
+  });
+
+  it(`Mario deve assinar o plano ${plan_remote_id}`, async () => {
+    // Utiliza esse mesmo token para criar um cartao em sua wallet
+
+    await request(app)
+      .post(`/customers/subscriptions`)
+      .set('Authorization', `Bearer ${data.token}`)
+      .send({
+        plan_id: plan_remote_id,
+        payment_method: 'credit_card',
+        card_id: data.card.remote_id,
+      })
+      .then(response => {
+        data.subscription = {
+          remote_id: response.body.id,
+          status: response.body.status,
+        };
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('start_at');
       });
   });
 });
