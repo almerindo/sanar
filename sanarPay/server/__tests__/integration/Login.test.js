3140915c5cb89154c7ec8af1fa0e0bada3ecc2b2
almerindo <almerindo.rehem@gmail.com>
2020-01-23 15:57:26 -0300

    feat: Novos testes implementados relacionados a usuarios e sessao  Changes to be committed: 	modified:   .env.test 	modified:   __tests__/coverage/lcov-report/controllers/CustomerController.js.html 	modified:   __tests__/coverage/lcov-report/controllers/PlanController.js.html 	modified:   __tests__/coverage/lcov-report/controllers/SessionController.js.html 	modified:   __tests__/coverage/lcov-report/controllers/SubscribeController.js.html 	modified:   __tests__/coverage/lcov-report/controllers/WalletController.js.html 	modified:   __tests__/coverage/lcov-report/controllers/index.html 	modified:   __tests__/coverage/lcov-report/index.html 	modified:   __tests__/coverage/lcov-report/middlewares/auth.js.html 	modified:   __tests__/coverage/lcov-report/middlewares/checkAdmin.js.html 	modified:   __tests__/coverage/lcov-report/middlewares/index.html 	modified:   __tests__/coverage/lcov-report/models/Card.js.html 	modified:   __tests__/coverage/lcov-report/models/Customer.js.html 	modified:   __tests__/coverage/lcov-report/models/Plan.js.html 	modified:   __tests__/coverage/lcov-report/models/Subscription.js.html 	modified:   __tests__/coverage/lcov-report/models/index.html 	modified:   __tests__/coverage/lcov.info 	modified:   __tests__/database.sqlite 	modified:   __tests__/integration/Login.test.js

--- sanarPay/server/__tests__/integration/Login.test.js
+++ sanarPay/server/__tests__/integration/Login.test.js
@@ -3,7 +3,7 @@ import app from '../../src/app';
 
 describe('SESSIONS', () => {
   const url = '/sessions';
-  const data = {};
+  let data = {};
   it('Deve requerer envio de dados no body', async () => {
     const response = await request(app)
       .post(url)
@@ -37,5 +37,86 @@ describe('SESSIONS', () => {
       .post(url)
       .send(data);
     expect(response.statusCode).toBe(401);
+  });
+
+  it('Deve Criar um Customer local e remoto (MundiPagG)', async () => {
+    data = {};
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+
+    const response = await request(app)
+      .post('/customers')
+      .send(data);
+    data.id = response.body.id;
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Deve impedir criar 2 usuários com o mesmo email', async () => {
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+
+    const response = await request(app)
+      .post('/customers')
+      .send(data);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('Deve retornar o token JWT apos usuário se logar', async () => {
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+
+    const response = await request(app)
+      .post('/sessions')
+      .send(data);
+    data.token = response.body.token;
+    console.log('response.body');
+    console.log(response.body);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('token');
+  });
+
+  it('Deve impedir cancelar um usuário se token não for informado', async () => {
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+
+    const response = await request(app)
+      .post(`/customers/${data.id}`)
+      .send(data);
+    console.log('response.body');
+    console.log(response.body);
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('Deve impedir cancelar um usuário se token for diferente do usuário a ser cancelado', async () => {
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwicmVtb3RlX2lkIjpudWxsLCJpYXQiOjE1Nzk3OTkzODAsImV4cCI6MTU4MDQwNDE4MH0.QLZrawVQjshOKGOFzaE38m_3uLU5AG-Y_cT6GOhz3j4';
+    const response = await request(app)
+      .delete(`/customers/${data.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(data);
+    console.log('response.body');
+    console.log(response.body);
+    expect(response.statusCode).toBe(405);
+  });
+
+  it('Deve permitir cancelar um usuário se token corresponder com o do usuário', async () => {
+    data.name = 'Almerindo Rehem';
+    data.email = 'almerindo.rehem@sanarflix.com.br';
+    data.password = '1234567890';
+    const response = await request(app)
+      .delete(`/customers/${data.id}`)
+      .set('Authorization', `Bearer ${data.token}`)
+      .send(data);
+    console.log('response.body');
+    console.log(response.body);
+    expect(response.statusCode).toBe(200);
   });
 });
