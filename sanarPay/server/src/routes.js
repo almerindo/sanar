b3b44cf3db397da0e13a78ae718dd501d2107c1f
almerindo <almerindo.rehem@gmail.com>
2020-01-22 21:04:07 -0300

    feature: modificado o padrao das rotas exemplo: routes.get('/customers/:cus/subscriptions/:subs', SubscribeController.index); // OK
    Retorna uma Subscription :subs do Customer :cus (todos os ID sao os ID remotos da MultipagG)

--- sanarPay/server/src/routes.js
+++ sanarPay/server/src/routes.js
@@ -19,23 +19,26 @@ routes.post('/sessions', SessionController.store); // OK
 // Para criar os planos precisa ser
 // TODO plano - Criar planos anual, trial 7dias, trimestral, promocional (com dois produtos e preço diferente a partir do primeiro mes)
 routes.post('/plans', checkAdminUser, PlanController.store); // OK
-routes.delete('/plans/:id', checkAdminUser, PlanController.delete); // OK
+routes.delete('/plans/:plan', checkAdminUser, PlanController.delete); // FIXME
 
 // Rotas que necessitem de autenticacao do cliente
 routes.use(authMiddleware); // OK
 
 // Cria cartao para um cliente ID
 routes.post('/customers/wallet', WalletController.store); // OK
-routes.delete('/customers/wallet', WalletController.delete); // OK
+routes.delete('/customers/:cus/wallet', WalletController.delete); // FIXME
 routes.get('/customers/wallet', WalletController.index); // OK
 
-routes.post('/customers/subscriptions', SubscribeController.store); // OK
-routes.get('/customers/subscriptions', SubscribeController.index); // OK
+routes.post('/customers/:cus/subscriptions', SubscribeController.store); // FIXME
+routes.get('/customers/subscriptions/:subs', SubscribeController.index); // OK
 routes.put('/customers/subscriptions', SubscribeController.update); // OK
-routes.delete('/customers/subscriptions', SubscribeController.delete); // OK
+routes.delete(
+  '/customers/:cus/subscriptions/:subs',
+  SubscribeController.delete
+); // FIXME
 
 // Customers Update de informações do usuário local e remoto
-routes.delete('/customers/:id', CustomerController.delete); // OK
+routes.delete('/customers/:cus', CustomerController.delete); // FIXME
 
 routes.put('/customers', CustomerController.update); // OK
 
